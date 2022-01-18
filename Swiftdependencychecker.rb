552c8d78d983ac111c990e1ab79a2b7ca0bca5de
Kristiina <kristiina.ra@gmail.com>
2022-01-18 14:18:12 +0200

    Update version to 0.1.2

--- Swiftdependencychecker.rb
+++ Swiftdependencychecker.rb
@@ -1,8 +1,8 @@
 class Swiftdependencychecker < Formula
   desc "Dependency checker for swift projects. Analyses dependencies declared in Podfile.lock, Cartfile.resolved and Package.resolved, queries the NDV database for vulnerabilities and outputs warnings for uses of libraries with known vulnerabilities. If tool is run as a build script phase in XCode then warnings are displayed in XCode."
   homepage "https://github.com/kristiinara/SwiftDependencyChecker"
-  url "https://github.com/kristiinara/SwiftDependencyChecker/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "af6d77331e7cc05814ce82d142530a26062e64efd42d0127294d1af22d8aab98"
+  url "https://github.com/kristiinara/SwiftDependencyChecker/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "02909925b9b410085a046302196ec133c622922d6ab75c29d0faaa8c19d48c6c"
   license "MIT"
 
   def install
